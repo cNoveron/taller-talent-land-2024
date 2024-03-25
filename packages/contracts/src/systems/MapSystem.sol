62f7348b76572fd7adfa7dea6e4ad9374f82e6f1
naikkuma <carlos.escom.ipn@gmail.com>
2024-03-24 21:22:55 -0600

    5. Agregamos la función para spawnear un jugador en el mapa

--- packages/contracts/src/systems/MapSystem.sol
+++ packages/contracts/src/systems/MapSystem.sol
@@ -2,6 +2,16 @@
 pragma solidity >=0.8.24;
 
 import { System } from "@latticexyz/world/src/System.sol";
+import { Movable, Player, Position } from "../codegen/index.sol";
+import { addressToEntityKey } from "../addressToEntityKey.sol";
 
 contract MapSystem is System {
+  function spawn(int32 x, int32 y) public {
+    bytes32 player = addressToEntityKey(address(_msgSender()));
+    require(!Player.get(player), "already spawned");
+
+    Player.set(player, true);
+    Position.set(player, x, y);
+    Movable.set(player, true);
+  }
 }
