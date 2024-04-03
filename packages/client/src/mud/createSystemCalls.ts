91dadb800dee303fa550b9e0577f1177bc868640
naikkuma <carlos.escom.ipn@gmail.com>
2024-04-03 15:12:20 -0600

    12.4. Agregamos el efecto de obstrucción en el cliente

--- packages/client/src/mud/createSystemCalls.ts
+++ packages/client/src/mud/createSystemCalls.ts
@@ -1,4 +1,4 @@
-import { getComponentValue } from "@latticexyz/recs";
+import { Has, HasValue, getComponentValue, runQuery } from "@latticexyz/recs";
 import { singletonEntity } from "@latticexyz/store-sync/recs";
 import { uuid } from "@latticexyz/utils";
 import { ClientComponents } from "./createClientComponents";
@@ -8,28 +8,13 @@ import { Direction } from "../direction";
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
 export function createSystemCalls(
-  /*
-   * The parameter list informs TypeScript that:
-   *
-   * - The first parameter is expected to be a
-   *   SetupNetworkResult, as defined in setupNetwork.ts
-   *
-   *   Out of this parameter, we only care about two fields:
-   *   - worldContract (which comes from getContract, see
-   *     https://github.com/latticexyz/mud/blob/main/templates/react/packages/client/src/mud/setupNetwork.ts#L63-L69).
-   *
-   *   - waitForTransaction (which comes from syncToRecs, see
-   *     https://github.com/latticexyz/mud/blob/main/templates/react/packages/client/src/mud/setupNetwork.ts#L77-L83).
-   *
-   * - From the second parameter, which is a ClientComponent,
-   *   we only care about Counter. This parameter comes to use
-   *   through createClientComponents.ts, but it originates in
-   *   syncToRecs
-   *   (https://github.com/latticexyz/mud/blob/main/templates/react/packages/client/src/mud/setupNetwork.ts#L77-L83).
-   */
   { playerEntity, worldContract, waitForTransaction }: SetupNetworkResult,
-  { Player, Position }: ClientComponents,
+  { Obstruction, Player, Position }: ClientComponents,
 ) {
+  const isObstructed = (x: number, y: number) => {
+    return runQuery([Has(Obstruction), HasValue(Position, { x, y })]).size > 0;
+  };
+
   const move = async (direction: Direction) => {
     if (!playerEntity) {
       throw new Error("no player");
@@ -50,6 +35,11 @@ export function createSystemCalls(
       y += 1;
     } else if (direction === Direction.West) {
       x -= 1;
+    }
+
+    if (isObstructed(x, y)) {
+      console.warn("cannot move to obstructed space");
+      return;
     }
 
     const positionId = uuid();
@@ -74,6 +64,11 @@ export function createSystemCalls(
     const canSpawn = getComponentValue(Player, playerEntity)?.value !== true;
     if (!canSpawn) {
       throw new Error("already spawned");
+    }
+
+    if (isObstructed(x, y)) {
+      console.warn("cannot spawn on obstructed space");
+      return;
     }
 
     const positionId = uuid();
