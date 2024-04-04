806e6d068176528257911e8ba517827e6803d100
naikkuma <carlos.escom.ipn@gmail.com>
2024-04-03 18:32:54 -0600

    18. Actualizar la lógica del tablero de juego

--- packages/client/src/GameBoard.tsx
+++ packages/client/src/GameBoard.tsx
@@ -1,11 +1,11 @@
-import { useComponentValue } from "@latticexyz/react";
+import { useComponentValue, useEntityQuery } from "@latticexyz/react";
 import { GameMap } from "./GameMap";
 import { useMUD } from "./MUDContext";
 import { useKeyboardMovement } from "./useKeyboardMovement";
 import { hexToArray } from "@latticexyz/utils";
 import { TerrainType, terrainTypes } from "./terrainTypes";
 import { singletonEntity } from "@latticexyz/store-sync/recs";
-import { Entity } from "@latticexyz/recs";
+import { Entity, Has, getComponentValueStrict } from "@latticexyz/recs";
 import { EncounterScreen } from "./EncounterScreen";
 import { MonsterType, monsterTypes } from "./monsterTypes";
 
@@ -20,25 +20,27 @@ export const GameBoard = () => {
 
   const canSpawn = useComponentValue(Player, playerEntity)?.value !== true;
 
-  const playerPosition = useComponentValue(Position, playerEntity);
-  const player =
-    playerEntity && playerPosition
-      ? {
-        x: playerPosition.x,
-        y: playerPosition.y,
-        emoji: "🤠",
-        entity: playerEntity,
-      }
-      : null;
+  const players = useEntityQuery([Has(Player), Has(Position)]).map((entity) => {
+    const position = getComponentValueStrict(Position, entity);
+    return {
+      entity,
+      x: position.x,
+      y: position.y,
+      emoji: entity === playerEntity ? "🤠" : "🥸",
+    };
+  });
 
   const mapConfig = useComponentValue(MapConfig, singletonEntity);
   if (mapConfig == null) {
-    throw new Error("map config not set or not ready, only use this hook after loading state === LIVE");
+    throw new Error(
+      "map config not set or not ready, only use this hook after loading state === LIVE"
+    );
   }
 
   const { width, height, terrain: terrainData } = mapConfig;
   const terrain = Array.from(hexToArray(terrainData)).map((value, index) => {
-    const { emoji } = value in TerrainType ? terrainTypes[value as TerrainType] : { emoji: "" };
+    const { emoji } =
+      value in TerrainType ? terrainTypes[value as TerrainType] : { emoji: "" };
     return {
       x: index % width,
       y: Math.floor(index / width),
@@ -47,8 +49,14 @@ export const GameBoard = () => {
   });
 
   const encounter = useComponentValue(Encounter, playerEntity);
-  const monsterType = useComponentValue(Monster, encounter ? (encounter.monster as Entity) : undefined)?.value;
-  const monster = monsterType != null && monsterType in MonsterType ? monsterTypes[monsterType as MonsterType] : null;
+  const monsterType = useComponentValue(
+    Monster,
+    encounter ? (encounter.monster as Entity) : undefined
+  )?.value;
+  const monster =
+    monsterType != null && monsterType in MonsterType
+      ? monsterTypes[monsterType as MonsterType]
+      : null;
 
   return (
     <GameMap
@@ -56,10 +64,13 @@ export const GameBoard = () => {
       height={height}
       terrain={terrain}
       onTileClick={canSpawn ? spawn : undefined}
-      players={player ? [player] : []}
+      players={players}
       encounter={
         encounter ? (
-          <EncounterScreen monsterName={monster?.name ?? "MissingNo"} monsterEmoji={monster?.emoji ?? "💱"} />
+          <EncounterScreen
+            monsterName={monster?.name ?? "MissingNo"}
+            monsterEmoji={monster?.emoji ?? "💱"}
+          />
         ) : undefined
       }
     />
